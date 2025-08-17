83c1b7c2e7f1c49196500fc80427444e2008e71a
Eric Matthes <ehmatthes@gmail.com>
2025-08-17 08:11:33 -0400

    Use --prefix env when setting env var.

--- dsd_platformsh/platform_deployer.py
+++ dsd_platformsh/platform_deployer.py
@@ -177,7 +177,8 @@ class PlatformDeployer:
         if dsd_config.settings_path.parts[-2:] == ("settings", "production.py"):
             plugin_utils.write_output("  Setting DJANGO_SETTINGS_MODULE environment variable...")
             dotted_settings_path = ".".join(dsd_config.settings_path.parts[-3:]).removesuffix(".py")
-            cmd = f"platform variable:create --level environment --environment . --name DJANGO_SETTINGS_MODULE --value {dotted_settings_path} --no-interaction"
+            # cmd = f"platform variable:create --level environment --environment . --name DJANGO_SETTINGS_MODULE --value {dotted_settings_path} --no-interaction"
+            cmd = f"platform variable:create --level environment --environment main --name DJANGO_SETTINGS_MODULE --value {dotted_settings_path} --no-interaction --visible-build true --prefix env"
             output = plugin_utils.run_quick_command(cmd)
             plugin_utils.write_output(output)
 
